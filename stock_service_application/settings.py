80478251157700c136ac6459397b96dc111e55de
Rikiza89 <149764397+Rikiza89@users.noreply.github.com>
2025-10-16 09:00:39 +0900

    Add files via upload

--- stock_service_application/settings.py
+++ stock_service_application/settings.py
@@ -60,9 +60,11 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'stock_service.context_processors.society_context',
             ],
         },
     },
